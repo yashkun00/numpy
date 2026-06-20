8b775c61c0cbe87bfd319edd077a67be62d46bdc
YASH WASULKAR <145082484+yashkun00@users.noreply.github.com>
2026-06-20 22:44:30 +0530

    Update np.p.py

--- np.p.py
+++ np.p.py
@@ -4,7 +4,7 @@ numpy in tensorflow, doing image prediction
 import numpy as np
 from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Densex = np.array([[1], [2], [3], [4]])y = np.array([[2], [4], [6], [8]])
-model = Sequential([Dense(1, input_shape=(1, ))])
+model = Sequential([Dense(12, input_shape=(2, ))])
 model.compile(optimizer='sgd', loss='mse')
-model.fit(x, y, epochs=100, verbose=0)
+model.fit(x, y, epochs=10, verbose=0)
 print(model.predict(np.array([[6]])))
