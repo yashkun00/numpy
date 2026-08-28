5a82333bb990c53b452258717bb1a3f548af8ba7
YASH WASULKAR <145082484+yashkun00@users.noreply.github.com>
2026-08-28 23:10:24 +0530

    A

--- np.p.py
+++ np.p.py
@@ -6,5 +6,5 @@ from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Densex = np.array([[1], [2], [3], [4]])y = np.array([[2], [4], [6], [8]])
 model = Sequential([Dense(1, input_shape=(2, ))])
 model.compile(optimizer='sgd', loss='mse')
-model.fit(x, y, epochs=5, verbose=0)
+model.fit(x, y, epochs=10, verbose=0)
 print(model.predict(np.array([[6]])))
